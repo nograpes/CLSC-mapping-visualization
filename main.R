c7340348d9078fc6d91af2878e9e0ff4977eca0d
Mon <yuanmengru@gmail.com>
2018-05-17 15:46:45 -0400

    Version 1.0

--- main.R
+++ main.R
@@ -46,33 +46,35 @@ server <- function(input, output){
         csd <- as.character(mapping_table$census_id[mapping_table[[input$type]] == input$option & mapping_table$census_type == "CSD"])
         ct <- as.character(mapping_table$census_id[mapping_table[[input$type]] == input$option & mapping_table$census_type == "CT"])
         da <- as.character(mapping_table$census_id[mapping_table[[input$type]] == input$option & mapping_table$census_type == "DA"])
-        
+
         if (length(cd) >=1) {
             plot <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(cd16.shp[cd16.shp$CDUID %in% c(cd),]) +
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2)
-        }
+        } 
+
         if (length(csd) >=1){
             plot <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(csd16.shp[csd16.shp$CSDUID %in% c(csd),])+
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2) 
         }
-        if (length(da) >=1){
+
+                if (length(da) >=1){
             plot <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(da16.shp[da16.shp$DAUID %in% c(da),])+
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2) 
         }
+
         if (length(ct) >=1){
             plot <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(ct16.shp[ct16.shp$CTUID %in% c(ct),])+
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2) 
         }
-    
-        tmap_leaflet(plot)
+    tmap_leaflet(plot)
     })
     output$table <- renderTable({mapping_table[mapping_table[[input$type]] == input$option,2:ncol(mapping_table)]})
 }
