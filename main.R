c9328bf643db8f13f6317d9b1fec5548d9a77804
Mon <yuanmengru@gmail.com>
2018-05-24 14:02:22 -0400

    final

--- main.R
+++ main.R
@@ -46,35 +46,39 @@ server <- function(input, output){
         csd <- as.character(mapping_table$census_id[mapping_table[[input$type]] == input$option & mapping_table$census_type == "CSD"])
         ct <- as.character(mapping_table$census_id[mapping_table[[input$type]] == input$option & mapping_table$census_type == "CT"])
         da <- as.character(mapping_table$census_id[mapping_table[[input$type]] == input$option & mapping_table$census_type == "DA"])
-
-        if (length(cd) >=1) {
-            plot <- tm_shape(clsc.shp) +
+       
+            if (length(cd) >=1) {
+            plot1 <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(cd16.shp[cd16.shp$CDUID %in% c(cd),]) +
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2)
         } 
-
-        if (length(csd) >=1){
-            plot <- tm_shape(clsc.shp) +
+        
+            if (length(csd) >=1){
+            plot1 <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(csd16.shp[csd16.shp$CSDUID %in% c(csd),])+
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2) 
         }
 
-                if (length(da) >=1){
-            plot <- tm_shape(clsc.shp) +
+            if (length(da) >=1){
+            plot1 <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(da16.shp[da16.shp$DAUID %in% c(da),])+
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2) 
         }
 
-        if (length(ct) >=1){
-            plot <- tm_shape(clsc.shp) +
+            if (length(ct) >=1){
+            plot1 <- tm_shape(clsc.shp) +
                 tm_polygons(border.col = "black", alpha = 0.1, lwd=1) +
                 tm_shape(ct16.shp[ct16.shp$CTUID %in% c(ct),])+
                 tm_polygons(border.col = "red", alpha = 0.2, lwd=2) 
         }
-    tmap_leaflet(plot)
+    # if else exist 
+        if( exists("plot1") )
+        {
+            tmap_leaflet(plot1)
+        }
     })
     output$table <- renderTable({mapping_table[mapping_table[[input$type]] == input$option,2:ncol(mapping_table)]})
 }
